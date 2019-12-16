c358b086c8299b671060fdb95efaf372dd2857be
Juri Em <juriem1974@gmail.com>
2019-12-16 12:41:54 +0200

    start with routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 
+const TopicsList = () => {
+    return (
+        <div>
+            <h1>Topics List</h1>
+        </div>
+    )
+};
+
+const TopicDetail = () => {
+    return (
+        <div>
+            <h1>Topic Detail</h1>
+        </div>
+    )
+};
+
 function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path='/' component={HomePage}/>
+                    <Route exact path='/topics' component={TopicsList}/>
+                    <Route path='/topics/:topicId' component={TopicDetail}/>
+                </Switch>
+            </div>
+        </Router>
+
+    );
 }
 
 export default App;
